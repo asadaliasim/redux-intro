8d61b32ccda3353036324fa01b4522bfd85d2cc6
Asad Ali Asim <asadaliasim909@gmail.com>
2024-05-20 15:43:54 +0500

    fix: layout and components

---
+++ src/Components/Counter/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Counter() {
+  return <>Hi from counter</>;
+}
+
+export default Counter;
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from '../../Components/Counter';
+
+function HomePage() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default HomePage;
