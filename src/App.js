30abae45908c95982bd415a73acc6ec388ae35e9
Asad Ali Asim <asadaliasim909@gmail.com>
2024-05-21 14:32:10 +0500

    eg: can access state any where in code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
   return (
     <div className="App">
       <HomePage />
+      <h1>{count}</h1>
     </div>
   );
 }
